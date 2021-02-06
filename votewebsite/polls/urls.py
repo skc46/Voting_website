b0d46ac612262bca68a1f42ba438564369e83632
skc46 <skc@crimson.ua.edu>
2021-02-06 15:01:58 -0700

    upto part 5, testing and generic views

--- votewebsite/polls/urls.py
+++ votewebsite/polls/urls.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-
 from django.urls import path
 
-from .views import (index, detail, results, vote) 
+#from .views import (index, detail, results, vote) 
+from . import views    # for generic views
 
 app_name = 'polls'  # this is helpful to navigate to URLs called Namespacing URL names
+# If app_name has defined then in template while hyperlinkig
+# {% url  app_name:urlname%}
+# urlpatterns = [
+#            path('', index, name='index'),
+#            path('<int:question_id>/', detail, name='detail'),
+#            path('<int:question_id>/results/', results, name='results'),
+#            path('<int:question_id>/vote/', vote, name='vote'),
+           
+#            ]
+
+""" To use class based views (which is more generic views), we change the 
+URLoatterns"""
 
 urlpatterns = [
-           path('', index, name='index_view'),
-           path('<int:question_id>/', detail, name='detail'),
-           path('<int:question_id>/results/', results, name='results'),
-           path('<int:question_id>/vote/', vote, name='vote'),
-           
-           ]
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name = 'results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    ]
--- votewebsite/polls/views.py
+++ votewebsite/polls/views.py
@@ -1,26 +1,82 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context ={'latest_question_list': latest_question_list}
+# def index(request):  # commented for creating class based view, see below
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context ={'latest_question_list': latest_question_list}
     
-    return render(request, 'polls/index.html', context)
+#     return render(request, 'polls/index.html', context)
 
+class IndexView(generic.ListView):  # equivalent of above function
+    template_name = 'polls/index.html'
+    context_object_name= 'latest_question_list'  # to change the default name
+    
+    def get_queryset(self):
+        """Return the last five published questions (
+        not including those set to be published in the future)."""
+        return Question.objects.filter(pub_date__lte = 
+                                       timezone.now()).order_by('-pub_date')[:5]
+           # lte means less than or equal to                            
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk = question_id)
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk = question_id)
         
-    context = {'question':question}    
-    return render(request, 'polls/detail.htm', context)
+#     context = {'question':question}    
+#     return render(request, 'polls/detail.html', context)
+""" CLass based view of detail"""
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    
+    """we want to avoid users from guessing future questions even if
+       they don't appear"""
+       
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+# def results(request, question_id):
 
+#     question = get_object_or_404(Question, pk = question_id)
+#     return render(request, 'polls/results.html', {'question': question})
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s." 
-    return HttpResponse(response % question_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+    
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice= question.choice_set.get(pk=request.POST['choice'])
+        # the request.POST['choice'] returns the ID of the selected choice
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form
+        context = {
+        'question': question,
+        'error_message': "You didn't select a choice.",
+            }    
+        return render(request, 'polls/detail.html', context)
+    
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        
+        # Always return an HttpResponseRedirect after succesfully dealing 
+        # with POST data. This prevents data from being posted twice
+        # if a user hits the Back button.
+        
+        return HttpResponseRedirect(reverse('polls:results', 
+                                            args=(question.id,)))
+      # The reverse() function is given the name of a view
+      # that we want to pass control to and the variable 
+      # portion of the URL pattern that points to that view.
+      # This means after somebody votes in question, the vote() view
+      # redirects to the results page for the question.
